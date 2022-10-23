af24e31226884b762118736b036ac031c3b297b3
shahrierNafis <shahriernafisog@gmail.com>
2022-10-23 15:10:03 +0600

    Create playRound function

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function getComputerChoice() {
     number = random3();
     if (number == 0) {
-        return "Rock";
+        return "rock";
     }
     else if (number == 1) {
-        return "Paper";
+        return "paper";
     }
-    return "Scissors";
+    return "scissors";
 }
+
 function random3() {
     while (true) {
         let number = Math.floor(Math.random() * 10);
@@ -17,4 +18,36 @@ function random3() {
         continue;
     }
 }
-console.log(getComputerChoice());
+const playerSelection = prompt().toLowerCase();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection == computerSelection){
+        return "draw";
+    }
+    else if(playerSelection == "rock"){
+        switch (computerSelection){
+            case "paper":
+                return "You Lose!";
+            case "scissors":
+                return "You Win!";
+        }
+    }
+    else if(playerSelection == "paper"){
+        switch (computerSelection){
+            case "rock":
+                return "You Win!";
+            case "scissors":
+                return "You Lose!";
+        }
+    }
+    else if(playerSelection == "scissors"){
+        switch (computerSelection){
+            case "paper":
+                return "You Win!";
+            case "rock":
+                return "You Lose!"
+        }
+    }   
+}
+console.log(playRound(playerSelection, computerSelection));
